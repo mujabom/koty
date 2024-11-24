3208cb136749d24b2aaf0fdf65b26a17a0f90f6d
mujahid bawazir <msbawazir.c@stc.com.sa>
2024-11-25 02:18:59 +0300

    oauth auto

--- src/main/kotlin/com/example/koty/Security/SecurityConfig.kt
+++ src/main/kotlin/com/example/koty/Security/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.web.cors.CorsConfiguration
 
 @Configuration
 @EnableWebSecurity
@@ -14,18 +15,33 @@ class SecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
-            cors {
-                disable()
+            cors { corsConfiguration() }
+            csrf { disable() }
+            httpBasic { disable() }
+            formLogin { disable() }
+            
+            oauth2Login {
+                loginPage = "/oauth2/authorization/google"
+                defaultSuccessUrl("/", true)
             }
-            csrf { }
-            httpBasic { }
-            formLogin { }
-            authorizeRequests { authorize("/hello", authenticated) }
-            authorizeRequests { authorize(anyRequest, permitAll) }
-
+            
+            authorizeRequests {
+                authorize("/", permitAll)
+                authorize("/error", permitAll)
+                authorize("/webjars/**", permitAll)
+                authorize("/oauth2/**", permitAll)
+                authorize(anyRequest, authenticated)
+            }
         }
 
         return http.build()
     }
+}
 
+fun corsConfiguration(): CorsConfiguration {
+    val corsConfiguration = CorsConfiguration()
+    corsConfiguration.allowedOrigins = listOf("http://localhost:3000")
+    corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+    corsConfiguration.allowedHeaders = listOf("*")
+    return corsConfiguration
 }
--- src/main/kotlin/com/example/koty/controller/Controller.kt
+++ src/main/kotlin/com/example/koty/controller/Controller.kt
@@ -2,6 +2,8 @@ package com.example.koty.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.user.OAuth2User
 
 @RestController("/")
 class Controller {
@@ -10,5 +12,10 @@ class Controller {
     @GetMapping("hello")
     fun getHello():String {
         return "hello"
+    }
+
+    @GetMapping("/user")
+    fun user(@AuthenticationPrincipal oauth2User: OAuth2User): MutableMap<String, Any>? {
+        return oauth2User.attributes
     }
 }
